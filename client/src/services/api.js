0e5c37b12a89afe68a03db660c6039845667164b
Sri-Sathwika <sathwika0112@gmail.com>
2026-05-31 19:27:37 +0530

    Backend fixed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "http://localhost:5000/api",
+    "https://mern-project-1-2b5e.onrender.com/",
 });
 
 // ADD TOKEN AUTOMATICALLY
